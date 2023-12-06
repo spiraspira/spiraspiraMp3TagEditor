7c7a4416b1524be4bb7a60a53cb2173401a1a3fd
spiraspira <zabornik5350@gmail.com>
2023-12-06 16:45:42 +0300

    Incapsulated methods to ToNumberTitleList (temporary)

--- HERKUSMONTAS88Mp3TagEditor/Extensions/StringListExtension.cs
+++ HERKUSMONTAS88Mp3TagEditor/Extensions/StringListExtension.cs
@@ -1,68 +1,52 @@
-﻿#pragma warning disable SYSLIB1045
-using static System.Runtime.InteropServices.JavaScript.JSType;
-using System.Xml.Linq;
-
-namespace HERKUSMONTAS88Mp3TagEditor.Extensions;
+﻿namespace HERKUSMONTAS88Mp3TagEditor.Extensions;
 
 /// <summary>
 /// Contains extension methods for <see cref="List{T}"/> of <see cref="string"/>.
 /// </summary>
 public static class StringListExtension
 {
-	public static readonly List<Regex> CorrectFileNamePatters = new()
-	{
-		
-		new(@"([0-9]+)\s(.+)"), //1 title.mp3
-		new(@"([0-9]+)\.\s(.+)"), //1. title.mp3
-		new(@"([0-9]+)-(.+)"), //1-title.mp3
-	};
-
-	public static Match? GetMatch(string input, List<Regex> patterns)
-	{
-		foreach (var pattern in patterns)
-		{
-			if (pattern.IsMatch(input))
-			{
-				return pattern.Match(input);
-			}
-		}
-
-		return null;
-	}
-
-	public static NumberTitle GetNumberTitle(string fileName, List<Regex> patterns)
-	{
-		uint number = 0;
-
-		string title = string.Empty;
-
-		var match = GetMatch(fileName, patterns);
-
-		if (match is null)
-		{
-			SharedEvents.InvokeNotify($"Incorrect file name: {fileName}. Couldn't set number and title.", false);
-		}
-		else
-		{
-			number = uint.Parse(match.Groups[1].Value);
-
-			title = match.Groups[2].Value;
-		}
-
-		return new(number, title);
-	}
-
 	/// <summary>
 	/// Converts <see cref="List{T}"/> of <see cref="string"/> expected to be
 	/// file paths to <see cref="List{T}"/> of <see cref="Tuple"/> (<see cref="uint"/>, <see cref="string"/>)/>
 	/// </summary>
 	/// <param name="files">File paths by specified rules.</param>
+	/// <param name="patterns">List of patterns.</param>
 	/// <returns>List of numbers and titles.</returns>
-	public static NumberTitleList ToNumberTitleList(this List<string> files)
+	public static NumberTitleList ToNumberTitleList(this List<string> files, List<Regex> patterns)
 	{
-		var fileNames = files.Select(Path.GetFileNameWithoutExtension).ToList();
+		return files
+			.Select(Path.GetFileNameWithoutExtension)
+			.Select(fileName => GetNumberTitle(fileName!, patterns))
+			.ToList();
 
-		return fileNames.Select(fileName => GetNumberTitle(fileName!, CorrectFileNamePatters)).ToList();
+		NumberTitle GetNumberTitle(string fileName, List<Regex> patterns)
+		{
+			uint number = default;
+
+			string title = string.Empty;
+
+			var match = GetMatch(fileName, patterns);
+
+			if (match is null)
+			{
+				SharedEvents.InvokeNotify($"Incorrect file name: {fileName}. Couldn't set number and title.", false);
+			}
+			else
+			{
+				number = uint.Parse(match.Groups[1].Value);
+
+				title = match.Groups[2].Value;
+			}
+
+			return new NumberTitle(number, title);
+
+			Match? GetMatch(string input, List<Regex> patterns)
+			{
+				return patterns
+					.FirstOrDefault(pattern => pattern.IsMatch(input))?
+					.Match(input);
+			}
+		}
 	}
 
 	/// <summary>
--- HERKUSMONTAS88Mp3TagEditor/Program.cs
+++ HERKUSMONTAS88Mp3TagEditor/Program.cs
@@ -1,4 +1,15 @@
-﻿SharedEvents.Notify += Log;
+﻿#pragma warning disable SYSLIB1045
+
+SharedEvents.Notify += Log;
+
+//https://regex-generator.olafneumann.org
+List<Regex> correctFileNamePatters = new()
+{
+
+	new(@"([0-9]+)\s(.+)"), //1 title.mp3
+	new(@"([0-9]+)\.\s(.+)"), //1. title.mp3
+	new(@"([0-9]+)-(.+)") //1-title.mp3
+};
 
 List<string>
 	audioExtensions = new() { "mp3", "wav", "flac" },
@@ -30,7 +41,7 @@ uint
 	year = ConsoleReader.ReadUInt32("Year: ");
 
 NumberTitleList
-	numbersAndTitles = audioFiles.ToNumberTitleList();
+	numbersAndTitles = audioFiles.ToNumberTitleList(correctFileNamePatters);
 
 List<uint>
 	numbers = numbersAndTitles.Select(t => t.Item1).ToList();
